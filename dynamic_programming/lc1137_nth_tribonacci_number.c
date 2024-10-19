1379743b36c6df1f555461f87b0071c0157b3ea8
Ishan Pranav <64936129+ishanpranav@users.noreply.github.com>
2024-10-19 19:20:09 -0400

    Update lc1137_nth_tribonacci_number.c

--- dynamic_programming/lc1137_nth_tribonacci_number.c
+++ dynamic_programming/lc1137_nth_tribonacci_number.c
@@ -1,22 +1,18 @@
-#include <stdlib.h>
-
 int tribonacci(int n) 
 {
     if (n < 1) { return 0; }
     if (n < 3) { return 1; }
 
-    int* t = malloc((n + 1) * sizeof * t);
-
-    t[0] = 0, t[1] = 1, t[2] = 1;
-
+    int t[3] = { 0, 1, 1 };
+    
     for (int i = 3; i <= n; i++)
     {
-        t[i] = t[i - 3] + t[i - 2] + t[i - 1];
+        int u = t[0] + t[1] + t[2];    
+    
+        t[0] = t[1];
+        t[1] = t[2];
+        t[2] = u;
     }
 
-    int a = t[n];
-
-    free(t);
-
-    return a;
+    return t[2];
 }
