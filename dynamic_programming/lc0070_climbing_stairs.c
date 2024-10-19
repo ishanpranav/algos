617c70fce07d10f315f945ef7febbe20ee2f043b
Ishan Pranav <64936129+ishanpranav@users.noreply.github.com>
2024-10-19 17:35:40 -0400

    Create lc0070_climbing_stairs.c

---
+++ dynamic_programming/lc0070_climbing_stairs.c
@@ -0,0 +1,20 @@
+int climbStairs(int n)
+{
+    if (n < 0) { return 0; }
+
+    int* p = malloc((n + 1) * sizeof * p);
+
+    p[0] = 1;
+    p[1] = 1;
+    
+    for (int i = 2; i <= n; i++)
+    {
+        p[i] = p[i - 1] + p[i - 2];
+    }
+
+    int a = p[n];
+
+    free(p);
+    
+    return a;
+}
