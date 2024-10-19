c4b5d6e60c5c5f38aa86e497ee9720c3a72a8eda
Ishan Pranav <64936129+ishanpranav@users.noreply.github.com>
2024-10-19 14:45:25 -0400

    Create lc0005_longest_palindromic_substring.c

---
+++ src/lc0005_longest_palindromic_substring.c
@@ -0,0 +1,39 @@
+char* longestPalindrome(char* s)
+{
+    size_t n = strlen(s);
+
+    if (!n) { return s; }
+
+    size_t ai = 0, ak = 1;
+    bool (*p)[n][n + 1] = calloc(n * (n + 1), sizeof(bool));
+
+    for (size_t i = 0; i < n; i++) { (*p)[i][1] = true; }
+
+    for (size_t i = 0; i < n - 1; i++)
+    {
+        if (s[i] != s[i + 1]) { continue; }
+        
+        (*p)[i][2] = true;
+        ai = i, ak = 2;
+    }
+
+    for (size_t k = 3; k <= n; k++)
+    {
+        for (size_t i = 0; i < n - (k - 1); i++)
+        {
+            if (s[i] != s[i + (k - 1)] || !(*p)[i + 1][k - 2]) { continue; }
+
+            (*p)[i][k] = true;
+
+            if (k <= ak) { continue; }
+
+            ai = i; ak = k;
+        }
+    }
+
+    free(p);
+
+    s[ai + ak] = '\0';
+
+    return s + ai;
+}
