379c8a5b94d7f806cc221d2894d94c6f82aeebf8
Ishan Pranav <64936129+ishanpranav@users.noreply.github.com>
2024-10-19 19:23:18 -0400

    Update lc0070_climbing_stairs.c

--- dynamic_programming/lc0070_climbing_stairs.c
+++ dynamic_programming/lc0070_climbing_stairs.c
@@ -1,21 +1,16 @@
-#include <stdlib.h>
-
-int climbStairs(int n)
+int climbStairs(int n) 
 {
     if (n < 0) { return 0; }
 
-    int* p = malloc((n + 1) * sizeof * p);
-
-    p[0] = 1, p[1] = 1;
+    int a[2] = { 1, 1 };
     
     for (int i = 2; i <= n; i++)
     {
-        p[i] = p[i - 2] + p[i - 1];
+        int b = a[0] + a[1];
+
+        a[0] = a[1];
+        a[1] = b;
     }
 
-    int a = p[n];
-
-    free(p);
-    
-    return a;
+    return a[1];
 }
